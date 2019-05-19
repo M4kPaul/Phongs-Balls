a71eea491868d680aba8c965185a9f35d4d5ebe6
Paweł Makowski <m4kpaul@gmail.com>
2019-05-20 00:23:21 +0200

    Add project files

---
+++ Phongs-Balls/MainWindow.xaml.cs
@@ -0,0 +1,118 @@
+﻿using System.Numerics;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace Phongs_Balls
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Image SelectedCanvas { get; set; }
+        private PhongSphere[] Spheres { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = PhongSphere.Presets;
+
+            Spheres = new PhongSphere[4]
+            {
+                PhongSphere.Presets[0],
+                PhongSphere.Presets[5],
+                PhongSphere.Presets[11],
+                PhongSphere.Presets[23]
+            };
+
+            Canvas1.Source = Spheres[0].RenderSphere();
+            Canvas2.Source = Spheres[1].RenderSphere();
+            Canvas3.Source = Spheres[2].RenderSphere();
+            Canvas4.Source = Spheres[3].RenderSphere();
+        }
+
+        private void Canvas_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Border border;
+            if (SelectedCanvas != null)
+            {
+                border = (SelectedCanvas.Parent as Border);
+                border.BorderThickness = new Thickness(1);
+                border.BorderBrush = new SolidColorBrush(Colors.White);
+            }
+            SelectedCanvas = (sender as Image);
+            border = SelectedCanvas.Parent as Border;
+            border.BorderThickness = new Thickness(2);
+            border.BorderBrush = new SolidColorBrush(Colors.Lime);
+            UpdateText();
+        }
+
+        private void Canvas_PreviewMouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                SelectedCanvas = (sender as Image);
+                var lightPosition = new Vector3((float)((-SelectedCanvas.ActualWidth / 2 + e.GetPosition(SelectedCanvas).X) * 0.01), (float)((-SelectedCanvas.ActualHeight / 2 + e.GetPosition(SelectedCanvas).Y) * 0.01), 2.5f);
+                Spheres[int.Parse(SelectedCanvas.Uid)].LightPosition = lightPosition;
+                SelectedCanvas.Source = Spheres[int.Parse(SelectedCanvas.Uid)].RenderSphere();
+                UpdateText();
+            }
+        }
+
+        private void ComboBoxPresets_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (SelectedCanvas != null)
+            {
+                Spheres[int.Parse(SelectedCanvas.Uid)] = (e.AddedItems[0] as PhongSphere);
+                if (!Spheres[int.Parse(SelectedCanvas.Uid)].IsRendered) Spheres[int.Parse(SelectedCanvas.Uid)].RenderSphere();
+                SelectedCanvas.Source = Spheres[int.Parse(SelectedCanvas.Uid)].Bitmap;
+                UpdateText();
+            }
+        }
+
+        private void Text_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (SelectedCanvas != null)
+            {
+                var lol = float.Parse(LX.Text);
+                if (float.TryParse(LX.Text, out var fLX) && float.TryParse(LY.Text, out var fLY) && float.TryParse(LZ.Text, out var fLZ) &&
+                    float.TryParse(LAr.Text, out var fLAr) && float.TryParse(LAg.Text, out var fLAg) && float.TryParse(LAb.Text, out var fLAb) &&
+                    float.TryParse(LDr.Text, out var fLDr) && float.TryParse(LDg.Text, out var fLDg) && float.TryParse(LDb.Text, out var fLDb) &&
+                    float.TryParse(LSr.Text, out var fLSr) && float.TryParse(LSg.Text, out var fLSg) && float.TryParse(LSb.Text, out var fLSb) &&
+                    float.TryParse(MAr.Text, out var fMAr) && float.TryParse(MAg.Text, out var fMAg) && float.TryParse(MAb.Text, out var fMAb) &&
+                    float.TryParse(MDr.Text, out var fMDr) && float.TryParse(MDg.Text, out var fMDg) && float.TryParse(MDb.Text, out var fMDb) &&
+                    float.TryParse(MSr.Text, out var fMSr) && float.TryParse(MSg.Text, out var fMSg) && float.TryParse(MSb.Text, out var fMSb) &&
+                    float.TryParse(Shi.Text, out var fShi))
+                {
+                    Spheres[int.Parse(SelectedCanvas.Uid)] = new PhongSphere("", new Vector3(fLX, fLY, fLZ),
+                                                                                 new Vector3(fLAr, fLAg, fLAb),
+                                                                                 new Vector3(fLDr, fLDg, fLDb),
+                                                                                 new Vector3(fLSr, fLSg, fLSb),
+                                                                                 new Vector3(fMAr, fMAg, fMAb),
+                                                                                 new Vector3(fMDr, fMDg, fMDb),
+                                                                                 new Vector3(fMSr, fMSg, fMSb),
+                                                                                 fShi);
+                    SelectedCanvas.Source = Spheres[int.Parse(SelectedCanvas.Uid)].RenderSphere();
+                }
+            }
+        }
+
+        private void UpdateText()
+        {
+            if (SelectedCanvas != null)
+            {
+                var sphere = Spheres[int.Parse(SelectedCanvas.Uid)];
+                LX.Text = sphere.LightPosition.X.ToString(); LY.Text = sphere.LightPosition.Y.ToString(); LZ.Text = sphere.LightPosition.Z.ToString();
+                LAr.Text = sphere.LightAmbient.X.ToString(); LAg.Text = sphere.LightAmbient.Y.ToString(); LAb.Text = sphere.LightAmbient.Z.ToString();
+                LDr.Text = sphere.LightDiffuse.X.ToString(); LDg.Text = sphere.LightDiffuse.Y.ToString(); LDb.Text = sphere.LightDiffuse.Z.ToString();
+                LSr.Text = sphere.LightSpecular.X.ToString(); LSg.Text = sphere.LightSpecular.Y.ToString(); LSb.Text = sphere.LightSpecular.Z.ToString();
+                MAr.Text = sphere.Ka.X.ToString(); MAg.Text = sphere.Ka.Y.ToString(); MAb.Text = sphere.Ka.Z.ToString();
+                MDr.Text = sphere.Kd.X.ToString(); MDg.Text = sphere.Kd.Y.ToString(); MDb.Text = sphere.Kd.Z.ToString();
+                MSr.Text = sphere.Ks.X.ToString(); MSg.Text = sphere.Ks.Y.ToString(); MSb.Text = sphere.Ks.Z.ToString();
+                Shi.Text = sphere.Shininess.ToString();
+            }
+        }
+    }
+}
---
+++ Phongs-Balls/PhongSphere.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Numerics;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Phongs_Balls
+{
+    class PhongSphere
+    {
+        readonly static int radius = 400;
+        readonly static int width = 1000;
+        readonly static int height = 1000;
+
+        public string Name { get; set; }
+        public Vector3 LightPosition { get; set; }
+        public Vector3 LightAmbient { get; set; }
+        public Vector3 LightDiffuse { get; set; }
+        public Vector3 LightSpecular { get; set; }
+        public Vector3 Ka { get; set; }
+        public Vector3 Kd { get; set; }
+        public Vector3 Ks { get; set; }
+        public float Shininess { get; set; }
+        public bool IsRendered { get; set; } = false;
+        public WriteableBitmap Bitmap = new WriteableBitmap(width, height, 96, 96, PixelFormats.Bgra32, null);
+
+        public static PhongSphere[] Presets { get; } = new PhongSphere[24]
+        {
+            new PhongSphere("emerald", new Vector3(0.0215f, 0.1745f, 0.0215f), new Vector3(0.07568f, 0.61424f, 0.07568f), new Vector3(0.633f, 0.727811f, 0.633f), 0.6f),
+            new PhongSphere("jade", new Vector3(0.135f, 0.2225f, 0.1575f), new Vector3(0.54f, 0.89f, 0.63f), new Vector3(0.316228f, 0.316228f, 0.316228f), 0.1f),
+            new PhongSphere("obsidian", new Vector3(0.05375f, 0.05f, 0.06625f), new Vector3(0.18275f, 0.17f, 0.22525f), new Vector3(0.332741f, 0.328634f, 0.346435f), 0.3f),
+            new PhongSphere("pearl", new Vector3(0.25f, 0.20725f, 0.20725f), new Vector3(1f, 0.829f, 0.829f), new Vector3(0.296648f, 0.296648f, 0.296648f), 0.088f),
+            new PhongSphere("ruby", new Vector3(0.1745f, 0.01175f, 0.01175f), new Vector3(0.61424f, 0.04136f, 0.04136f), new Vector3(0.727811f, 0.626959f, 0.626959f), 0.6f),
+            new PhongSphere("turquoise", new Vector3(0.1f, 0.18725f, 0.1745f), new Vector3(0.396f, 0.74151f, 0.69102f), new Vector3(0.297254f, 0.30829f, 0.306678f), 0.1f),
+            new PhongSphere("brass", new Vector3(0.329412f, 0.223529f, 0.027451f), new Vector3(0.780392f, 0.568627f, 0.113725f), new Vector3(0.992157f, 0.941176f, 0.807843f), 0.21794872f),
+            new PhongSphere("bronze", new Vector3(0.2125f, 0.1275f, 0.054f), new Vector3(0.714f, 0.4284f, 0.18144f), new Vector3(0.393548f, 0.271906f, 0.166721f), 0.2f),
+            new PhongSphere("chrome", new Vector3(0.25f, 0.25f, 0.25f), new Vector3(0.4f, 0.4f, 0.4f), new Vector3(0.774597f, 0.774597f, 0.774597f), 0.6f),
+            new PhongSphere("copper", new Vector3(0.19125f, 0.0735f, 0.0225f), new Vector3(0.7038f, 0.27048f, 0.0828f), new Vector3(0.256777f, 0.137622f, 0.086014f), 0.1f),
+            new PhongSphere("gold", new Vector3(0.24725f, 0.1995f, 0.0745f), new Vector3(0.75164f, 0.60648f, 0.22648f), new Vector3(0.628281f, 0.555802f, 0.366065f), 0.4f),
+            new PhongSphere("silver", new Vector3(0.19225f, 0.19225f, 0.19225f), new Vector3(0.50754f, 0.50754f, 0.50754f), new Vector3(0.508273f, 0.508273f, 0.508273f), 0.4f),
+            new PhongSphere("black plastic", new Vector3(0f, 0f, 0f), new Vector3(0.01f, 0.01f, 0.01f), new Vector3(0.5f, 0.5f, 0.5f), 0.25f),
+            new PhongSphere("cyan plastic", new Vector3(0f, 0.1f, 0.06f), new Vector3(0f, 0.50980392f, 0.50980392f), new Vector3(0.50196078f, 0.50196078f, 0.50196078f), 0.25f),
+            new PhongSphere("green plastic", new Vector3(0f, 0f, 0f), new Vector3(0.1f, 0.35f, 0.1f), new Vector3(0.45f, 0.55f, 0.45f), 0.25f),
+            new PhongSphere("red plastic", new Vector3(0f, 0f, 0f), new Vector3(0.5f, 0f, 0f), new Vector3(0.7f, 0.6f, 0.6f), 0.25f),
+            new PhongSphere("white plastic", new Vector3(0f, 0f, 0f), new Vector3(0.55f, 0.55f, 0.55f), new Vector3(0.7f, 0.7f, 0.7f), 0.25f),
+            new PhongSphere("yellow plastic", new Vector3(0f, 0f, 0f), new Vector3(0.5f, 0.5f, 0f), new Vector3(0.6f, 0.6f, 0.5f), 0.25f),
+            new PhongSphere("black rubber", new Vector3(0.02f, 0.02f, 0.02f), new Vector3(0.01f, 0.01f, 0.01f), new Vector3(0.4f, 0.4f, 0.4f), 0.078125f),
+            new PhongSphere("cyan rubber", new Vector3(0f, 0.05f, 0.05f), new Vector3(0.4f, 0.5f, 0.5f), new Vector3(0.04f, 0.7f, 0.7f), 0.078125f),
+            new PhongSphere("green rubber", new Vector3(0f, 0.05f, 0f), new Vector3(0.4f, 0.5f, 0.4f), new Vector3(0.04f, 0.7f, 0.04f), 0.078125f),
+            new PhongSphere("red rubber", new Vector3(0.05f, 0f, 0f), new Vector3(0.5f, 0.4f, 0.4f), new Vector3(0.7f, 0.04f, 0.04f), 0.078125f),
+            new PhongSphere("white rubber", new Vector3(0.05f, 0.05f, 0.05f), new Vector3(0.5f, 0.5f, 0.5f), new Vector3(0.7f, 0.7f, 0.7f), 0.078125f),
+            new PhongSphere("yellow rubber", new Vector3(0.05f, 0.05f, 0f), new Vector3(0.5f, 0.5f, 0.4f), new Vector3(0.7f, 0.7f, 0.04f), 0.078125f)
+        };
+
+        public PhongSphere(string name, Vector3 ka, Vector3 kd, Vector3 ks, float shininess) : this(name, new Vector3(1, -1, 2.5f), new Vector3(1, 1, 1), new Vector3(1, 1, 1), new Vector3(1, 1, 1), ka, kd, ks, shininess) { }
+
+        public PhongSphere(string name, Vector3 lightPosition, Vector3 lightAmbient, Vector3 lightDiffuse, Vector3 lightSpecular, Vector3 ka, Vector3 kd, Vector3 ks, float shininess)
+        {
+            Name = name;
+            LightPosition = lightPosition;
+            LightAmbient = lightAmbient;
+            LightDiffuse = lightDiffuse;
+            LightSpecular = lightSpecular;
+            Ka = ka;
+            Kd = kd;
+            Ks = ks;
+            Shininess = shininess;
+        }
+
+        public WriteableBitmap RenderSphere()
+        {
+            var pixels = new uint[width * height];
+            for (var y = -radius; y <= radius; y++)
+            {
+                for (var x = -radius; x <= radius; x++)
+                {
+                    var red = 16;
+                    var green = 16;
+                    var blue = 16;
+                    var alpha = 255;
+
+                    if (x * x + y * y < radius * radius)
+                    {
+                        var z = (float)Math.Sqrt(radius * radius - x * x - y * y);
+                        var point = new Vector3(x, y, z);
+                        var pointN = Vector3.Normalize(point);
+                        var normal = Vector3.Normalize(new Vector3(x, y, z));
+                        var V = Vector3.Normalize(new Vector3(0, 0, 0) - point);
+                        var L = LightPosition - pointN;
+                        var LN = Vector3.Normalize(L);
+                        var R = normal * (2 * Vector3.Dot(normal, LN)) - LN;
+
+                        var diffuseFactor = Vector3.Dot(L, normal);
+                        var specularFactor = Math.Pow(Vector3.Dot(R, V), (int)(Shininess * 128));
+
+                        red = Math.Min(Math.Max((int)((LightAmbient.X * Ka.X + (LightDiffuse.X * Kd.X * diffuseFactor) + (LightSpecular.X * Ks.X * specularFactor)) * 255), 0), 255);
+                        green = Math.Min(Math.Max((int)((LightAmbient.Y * Ka.Y + (LightDiffuse.Y * Kd.Y * diffuseFactor) + (LightSpecular.Y * Ks.Y * specularFactor)) * 255), 0), 255);
+                        blue = Math.Min(Math.Max((int)((LightAmbient.Z * Ka.Z + (LightDiffuse.Z * Kd.Z * diffuseFactor) + (LightSpecular.Z * Ks.Z * specularFactor)) * 255), 0), 255);
+                    }
+
+                    pixels[(y + radius + 100) * width + x + radius + 100] = (uint)((alpha << 24) + (red << 16) + (green << 8) + blue);
+                }
+            }
+
+            Bitmap.WritePixels(new Int32Rect(0, 0, width, height), pixels, width * 4, 0);
+            IsRendered = true;
+            return Bitmap;
+        }
+    }
+}
